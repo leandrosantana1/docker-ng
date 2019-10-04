cf9fe85802ce044a109bfef4e977c62f574ea438
Patrice Ferlet <metal3d@gmail.com>
2019-10-04 13:55:18 +0200

    Add version 8
    - compiled all 8.x versions
    - better python script to manage versions in README file

--- manage-readme-ng-version.py
+++ manage-readme-ng-version.py
@@ -1,19 +1,23 @@
 import subprocess
+import sys
+from collections import OrderedDict
+
+LATEST = '[latest](https://github.com/metal3d' + \
+         '/docker-ng/blob/master/src/Dockerfile)'
 
 images = subprocess.Popen(
         ["docker", "images", "metal3d/ng",
             "--format", "{{ .Tag }} {{ .ID }} {{ .Repository }}"],
         stdout=subprocess.PIPE)
 sort = subprocess.Popen(
-        ["sort", "-V"],
+        ["sort", "-V", "-r"],
         stdin=images.stdout,
         stdout=subprocess.PIPE)
 
 images.stdout.close()
 versions = sort.communicate()[0].decode().strip().split('\n')
-versions.sort(reverse=True)
 
-releases = {}
+releases = OrderedDict()
 for v in versions:
     version, imid, repo = v.split(' ')
     if 'docker.io' in repo:
@@ -22,21 +26,10 @@ for v in versions:
     if imid not in releases:
         releases[imid] = []
 
+    if 'latest' in version:
+        version = LATEST
+
     releases[imid].append(version)
 
-for idx, (_, versions) in enumerate(releases.items()):
-    versions = list(reversed(versions))
-    versions = ['`%s`' % v for v in versions if v != '<none>']
-
-    if len(versions) == 0:
-        continue
-
-    end = ''
-    if len(versions) > 1:
-        end = '\n'
-
-    if idx == 0:
-        versions[-1] = '[latest](https://github.com/metal3d' + \
-                       '/docker-ng/blob/master/src/Dockerfile)'
-
-    print(end + '- ' + ' -> '.join(versions))
+for _, v in releases.items():
+    print('- ' + (' -> '.join(v)))
